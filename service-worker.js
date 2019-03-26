553fc9dac0218970a9d3f843f76dafb4f2d112b0
Connor Bach <cbach120@gmail.com>
2019-03-26 15:33:43 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ConnorBach/nd_dh_react/precache-manifest.40b446db00008f6eadefe5d7dc102af8.js"
+  "/nd_dh_react/precache-manifest.0e1051c933201bbd712cdb3a1ca39f13.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ConnorBach/nd_dh_react/index.html", {
+workbox.routing.registerNavigationRoute("/nd_dh_react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
