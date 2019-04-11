28c32b0872846c18c99fa0f0b1ac8be9164e32e9
Connor Bach <cbach120@gmail.com>
2019-04-11 11:48:25 +0100

    adding dynamic api urls with env vars

--- src/api/apiFunctions.js
+++ src/api/apiFunctions.js
@@ -1,13 +1,18 @@
-const apiURL = "http://www.dh-rest-service.herokuapp.com";
+const apiURL = 'https://www.dh-rest-service.herokuapp.com';
+const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT || apiURL;
 export const getTodayFoods = async () => {
-  console.log("hitting api");
-  const res = await fetch("/api/today", {
+  console.log('hitting api');
+  console.log('process.endpoint: ', process.env.REACT_APP_API_ENDPOINT);
+  console.log('actual endpoint: ', API_ENDPOINT);
+  const res = await fetch(`${API_ENDPOINT}/api/today`, {
     headers: {
-      accepts: "application/json"
-    }
+      accepts: 'application/json',
+    },
   });
-  console.log("res", res);
+  console.log('res', res);
   const json = await res.json();
   console.log(json);
   return json;
 };
+
+export { getTodayFoods as default };
